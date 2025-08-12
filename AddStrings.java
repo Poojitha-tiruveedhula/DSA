bc62d0a269653248626da9272133e7b3ce0bfe53
Poojitha-tiruveedhula <poojithatiruveedhula@gmail.com>
2025-08-12 08:42:53 +0530

    Adding strings in java
    Adding strings in java

---
+++ AddStrings.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.util.Scanner;
+public class AddStrings
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+String num1=sc.nextLine();
+String num2=sc.nextLine();
+System.out.println(addStrings(num1,num2));
+}
+public static String addStrings(String num1,String num2)
+{
+StringBuilder sb=new StringBuilder();
+int i=num1.length()-1;
+int j=num2.length()-1;
+int carry=0;
+while(i>=0 || j>=0 || carry!=0)
+{
+int digit1=i>=0?num1.charAt(i)-'0':0;
+int digit2=j>=0?num2.charAt(j)-'0':0;
+int sum=digit1+digit2+carry;
+ carry=sum/10;
+sb.append(sum%10);
+i--;
+j--;
+}
+return sb.reverse().toString();
+}
+}
+
